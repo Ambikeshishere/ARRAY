8a401e319462ab348657f3d8c207983f1fb5fc56
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-16 15:37:26 +0530

    Remove Duplicates from Sorted Arrays

--- 4.py
+++ 4.py
@@ -1,1 +1,11 @@
-#DSA 
+#Remove Duplicates from Sorted Arrays
+
+class solution :
+    nums = input("Enter your number \n ")
+    def removeDuplicates(a, nums: list[int]) -> int:
+        j = 1
+        for i in range(1 , len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[j] = nums[i]
+                j = j+1
+        return j
