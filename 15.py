c59b392f18d9aeb7a3dacb5d6139873d6a35f34f
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-19 18:09:16 +0530

    Marriage age check

---
+++ 15.py
@@ -0,0 +1,8 @@
+#Marriage age check
+
+age = input("Biyaaah karne ki kabiliyat pata karne ke liye umra darj kare \n")
+age = float(age)
+if (age >= 18):
+    print("Mubarak ho aap kabil hai jaaiye nayi nayi lugayi dhudiye :-D \n ")
+else:
+    print("Bhagg jaa yaha se kutta kahi ka \n")
