d4c3324644c96cbc9074529accdb0095887dde44
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-19 23:57:24 +0530

    WAP to count the numbers of students with the "A" Grade in the following tuple.

--- 24.py
+++ 24.py
@@ -3,3 +3,8 @@
 Grades = ("C","D","A","A","B","B","A")
 
 print(Grades.count("A"))
+
+#WAP to write the grades in List and sort it.
+Grades_list = ["C","D","A","A","B","B","A"]
+Grades_list.sort()
+print(Grades_list)
