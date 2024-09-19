8439ecf1b43b61b1452433f748375627750fbf29
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-19 14:46:45 +0530

    WAP Tp input user's first name and print its length.

--- 14.py
+++ 14.py
@@ -5,5 +5,5 @@ print(len(str))
 
 #WAP to find occurrence of '$' in a string
 
-str2 = ("I have $ bcoz my father is AMBANI. I act like a KR $ sign ")
+str2 = ("I have $ bcoz my father is king. I act like a KR $ sign ")
 print(str2.count("$"))
