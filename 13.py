f6deb168edf21bcf1364057eefdb78a435c048e4
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-19 14:40:51 +0530

    string functions

--- 13.py
+++ 13.py
@@ -1,5 +1,9 @@
+
+#string functions
 str = "i am Ambikesh Srivastava"
 print(str.endswith("ava"))
 print(str.capitalize())
 print(str)
 print(str.replace("Ambikesh", "Abhay" ))
+print(str.find("v"))
+print(str.count("a"))
