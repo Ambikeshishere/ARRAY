49a142e4ba86f8f206494cd4bcc4b04f7bd64808
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-19 21:28:37 +0530

    WAP to check if a number entered by the user is odd or even.

---
+++ 17.py
@@ -0,0 +1,10 @@
+#WAP to check if a number entered by the user is odd or even.
+
+number = input("Enter the fucking number \n")
+
+number = float(number)
+
+if (number % 2 == 0):
+    print("EVEN Number hai")
+else:
+    print("ODD number hai")
