61acf168bd9791e443a29110708ff5d02ea06c3b
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-19 23:33:47 +0530

    Tuples

---
+++ 21.py
@@ -0,0 +1,10 @@
+tup = (87,45,12,78,45,798,54,12,14,45,56,12,85,45)
+tup1 = (1,)
+print(tup[0])
+print(tup)
+print(tup1)
+print(type(tup))
+print(type(tup1))
+print(tup[1:5]) #Slicing of tuples
+print(tup.index(12))
+print(tup.count(12))
