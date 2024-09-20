6826b38e1dd73e57f28416864ac3b178a6552686
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-20 21:59:36 +0530

    Basic  Question

--- 27.py
+++ 27.py
@@ -1,2 +1,15 @@
 # Create a dictionary
 
+dictionary = {
+    "table" : ["a piece of furniture", "list of facts and figures"],
+    "cat" : "a small animal"
+}
+
+print(dictionary)
+
+
+# sets related question
+
+students = {"python", "java", "c++", "python","javascript","java","python","java","c++","C"}
+print(students)
+print("Total number of Classroom is :- ",len(students))
