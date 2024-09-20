da840e555faa770e86a4c815ae51fa03878a6842
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-20 13:27:50 +0530

    SETS

---
+++ 26.py
@@ -0,0 +1,20 @@
+#SETS
+
+
+sets = {1,2,3,4,5,6,"Ambikesh","Srivastava",4,45,8,3,6}
+print(sets)
+print(len(sets))
+null__sets = set()
+print(type(sets))
+print(type(null__sets))
+null__sets.add(1)
+null__sets.add(2)
+sets.remove("Ambikesh")
+sets.clear()
+
+
+
+
+print(sets)
+print(null__sets)
+print(null__sets.pop())
