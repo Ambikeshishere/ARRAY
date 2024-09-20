9bffe6cdc5e8a7797d5a42616b22fd9c573b1fa2
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-20 12:22:12 +0530

    Information

---
+++ 25.py
@@ -0,0 +1,12 @@
+#Information
+
+info = {
+    "name" : "Ambikesh Srivastava",
+    "age": 20,
+    "is_adult": True,
+    "height": 5.6,
+    "race": "Black"
+    }
+
+print(info)
+print(info["name"])
