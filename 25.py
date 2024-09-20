3b6c5eaf6d1a90059472177014d668bbd76827ed
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-20 12:38:00 +0530

    Information

--- 25.py
+++ 25.py
@@ -5,7 +5,17 @@ info = {
     "age": 20,
     "is_adult": True,
     "height": 5.6,
-    "race": "Black"
+    "race": "Black",
+    "skills" : {
+        "Java" : "Intermediate",
+        "Python": 'Advanced',
+        "C": "Intermediate",
+        "C++": "Basic",
+        "HTML": "Advanced",
+        "CSS" : "Advanced",
+        "JavaScript": "Advanced",
+        "ReactJs": "Intermediate"
+    }
     }
 
 print(info)
@@ -14,3 +24,8 @@ info["name"] = "Abhay Raj LALA"
 
 print(info)
 print(type(info))
+print(info["skills"])
+print(info["skills"]["Java"])
+print(info.keys())
+print(list(info.keys()))
+print(len(list(info.keys())))
