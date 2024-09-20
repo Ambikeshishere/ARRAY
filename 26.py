ad1c132e5b70c9eb99e9018b6bd25c1c89b466f5
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-20 21:47:20 +0530

    SETS

--- 26.py
+++ 26.py
@@ -2,6 +2,7 @@
 
 
 sets = {1,2,3,4,5,6,"Ambikesh","Srivastava",4,45,8,3,6}
+sets2 = {1,5,1,6,1,8,4,8,7,54,465,47,87,5,564,84,87}
 print(sets)
 print(len(sets))
 null__sets = set()
@@ -19,3 +20,5 @@ print(sets)
 print(null__sets)
 print(null__sets.pop())
 print(null__sets) 
+
+print(sets.union(sets2))
