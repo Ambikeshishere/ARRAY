25f93bb0ab058174e637fe8a8585d0c37e52534b
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-21 11:33:22 +0530

    Print Hello World 5 times using While loops

---
+++ 30.py
@@ -0,0 +1,9 @@
+# Print Hello World 5 times using While loops
+a =  input("Enter your limit to print hello world\n")
+
+a = float(a)
+count = 1
+while count <= a:
+    print("Hello world")
+    count += 1
+    
