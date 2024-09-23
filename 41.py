12bb2d977ab9ffadaefcc59db54f5408a2766b00
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-23 16:18:27 +0530

    WAF to print the length of a list. (list is the parameter)

--- 41.py
+++ 41.py
@@ -1,9 +1,12 @@
 # WAF to print the length of a list. (list is the parameter)
 
 cities = ["Delhi", "Noida", "Prayagraj", "Ahmedabad"]
+students = ["Ambikesh", "Abhay", "Aditya", "Arohi", "Pooja", "Devanshi"]
 
 def print_length(list):
     print(len(list))
     return list
 
 print_length(cities)
+
+print_length(students)
