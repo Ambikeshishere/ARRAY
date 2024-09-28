c3545d50eeeda79e20fd095399bad1d20046c49a
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-28 23:04:20 +0530

    init function

--- 51.py
+++ 51.py
@@ -1,1 +1,11 @@
 # init function
+
+class student:
+    name = "Ambikesh"
+    branch = "ICE"
+    def __init__(self):
+        print("Loading DATABASE.................")
+
+s1 = student()
+print(s1.name)
+print(s1.branch)
