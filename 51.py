7b20a6df1f141a09cef74aa496dd1e085876c38a
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-29 11:43:23 +0530

    init function

--- 51.py
+++ 51.py
@@ -1,11 +1,16 @@
 # init function
 
 class student:
-    
+    #default constructors
+    def __init__(self):        
+        print("Loading DATABASE.................")
+    #paramaterized constructors
     def __init__(self, fullname, marks):
         self.name = fullname
         self.marks = marks
         print("Loading DATABASE.................")
+
+        
 new = input("Enter the name of student\n")
 mark = input("Enter the name of student\n")
 s1 = student(new, mark)
