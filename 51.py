ad4c43746e138740e511f5574df463b36b2c92fd
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-29 11:41:02 +0530

    init function

--- 51.py
+++ 51.py
@@ -2,9 +2,14 @@
 
 class student:
     
-    def __init__(self, fullname):
+    def __init__(self, fullname, marks):
         self.name = fullname
+        self.marks = marks
         print("Loading DATABASE.................")
 new = input("Enter the name of student\n")
-s1 = student(new)
+mark = input("Enter the name of student\n")
+s1 = student(new, mark)
 print(s1.name)
+print(s1.marks)
+
+print(s1.name,s1.marks)
