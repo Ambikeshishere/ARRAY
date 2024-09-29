9ef8126126a23cb810b0c92482a91fcbc3fe1aea
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-29 23:47:17 +0530

    Private Attributes and methods

--- 57.py
+++ 57.py
@@ -2,9 +2,13 @@
 class Account:
     def __init__ (self, acc_number, acc_password):
         self.acc_number = acc_number
-        self.acc_password = acc_password
+        self.__acc_password = acc_password
+
+    def reset_pass(self):
+        print(self.__acc_password)
 
 acc1 = Account("12345","asdfgh")
 
 print(acc1.acc_number)
-print(acc1.acc_password)
+
+print(acc1.reset_pass())
