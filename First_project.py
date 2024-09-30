9f10bbcea73fead0057777667b389b14bf3194a1
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-30 16:14:42 +0530

    Cafe Billing and order system

--- First_project.py
+++ First_project.py
@@ -14,3 +14,8 @@ menu = {
     'Long Black Coffee': 245,
     'Blonde': 999
 }
+
+print("Welcome to STARK CAFE's")
+print("Espresso : $150\nRistretto : $90\nLatte: $120\nMacchiato: $250\nMocha: $450\nAmericano: $850\nold Brew: $340\nNitro Cold Brew: $550\nFlat White: $250\nIced Coffee: $399\nLong Black Coffee: $245\nBlonde: $999")
+
+order_total = 0
