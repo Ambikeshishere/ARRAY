1247d6ad5398a61dbadcd871169371e22dcea17a
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-09-30 16:05:33 +0530

    Cafe Billing and order system

---
+++ First_project.py
@@ -0,0 +1,16 @@
+# Cafe Billing and order system
+
+menu = {
+    'Espresso' : 150,
+    'Ristretto' : 90,
+    'Latte':120,
+    'Macchiato':250,
+    'Mocha':450,
+    'Americano':850,
+    'Cold Brew':340,
+    'Nitro Cold Brew':550,
+    'Flat White':250,
+    'Iced Coffee':399,
+    'Long Black Coffee': 245,
+    'Blonde': 999
+}
