1571b3e2ddc9d68e6fae9aabc5b92b69af8f3f0f
Ambikesh Srivastava <109055815+Ambikeshishere@users.noreply.github.com>
2024-10-01 15:01:42 +0530

    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.

---
+++ DSA5.py
@@ -0,0 +1,22 @@
+'''Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.'''
+
+class Solution:
+    def isValid( s: str) -> bool:
+        stack = []
+        for c in s:
+            if c in '({[':
+                stack.append(c)
+            else:
+                if not stack or \
+                    (c == ')' and stack[-1] != '(') or \
+                    (c == '}' and stack[-1] != '{') or \
+                    (c == ']' and stack[-1] != '['):
+                    return False
+                stack.pop()
+        return not stack
+    
+x = input("Given a string s containing just the characters '(', ')', '{', '}', '[' and ']' \n")
+print(Solution.isValid(x))
+
+
+
