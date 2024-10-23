905db7967d3fc941d93a4cff4742a5e0f817a63a
Ambikesh Srivastava <abhay2004raj15@gmail.com>
2024-10-23 19:11:30 +0530

    Combinations of Sums

---
+++ DSA35.py
@@ -0,0 +1,24 @@
+# Combination of sums
+class Solution:
+    def combinationSum2(candidates, target):
+        candidates.sort()
+        res = []
+
+        def dfs (target, start, comb):
+            if target < 0:
+                return
+            if target == 0:
+                res.append(comb)
+                return
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates [i-1]:
+                    continue
+                if candidates[i] > target:
+                    break
+                dfs(target - candidates[i], i+1, comb + [candidates[i]])
+        dfs(target, 0, [])
+        return res
+x = [10,1,2,7,6,1,5]
+t = 8
+s = Solution.combinationSum2(x,t)
+print(s)
