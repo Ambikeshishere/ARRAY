f27c03a268b9e9a3cb4be192c1e5437c1c533fab
Ambikesh Srivastava <abahy2004raj15@gmail.com>
2024-12-22 14:02:18 +0530

    Update Karo

--- 1.py
+++ 1.py
@@ -2,3 +2,4 @@ print("Hello World \n")
 print("I am Ambikesh Srivastava \n")
 print("I am in B.Tech Final Year and my trade is Instrumentation and Control Engineering \n")
 #First Program
+print("Windows is one of the worst Os in this world.")
