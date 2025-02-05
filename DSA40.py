e6b6da1dbe413efc54026b869629f1270c8d1645
Ambikesh Srivastava <abhay2004raj15@gmail.com>
2025-02-05 14:31:49 +0530

    Import Update

---
+++ DSA40.py
@@ -0,0 +1,25 @@
+# Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+from collections import deque
+class Solution:
+    def levelOrder(self, root: TreeNode):
+        if not root:
+            return []
+        result = []
+        queue = deque([root])
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level)
+        return result
